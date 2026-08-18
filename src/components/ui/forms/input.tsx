b196a543b99b28cc0d4273e760b14eeef60ba2b1
FCL00 <flagahit00@gmail.com>
2026-08-18 21:32:55 +0800

    feature: add new ui for forms: error message, field-wrapper, label, and input

---
+++ src/components/ui/forms/input.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { type UseFormRegisterReturn } from 'react-hook-form';
+
+import { FieldWrapper, FieldWrapperPassThroughProps } from './field-wrapper';
+import { cn } from '@/utils/cn';
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  FieldWrapperPassThroughProps & {
+    className?: string;
+    registration: Partial<UseFormRegisterReturn>;
+  };
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, label, error, registration, ...props }, ref) => {
+    return (
+      <FieldWrapper label={label} error={error}>
+        <input
+          type={type}
+          className={cn(
+            'w-full bg-surface text-[14px] p-[12px_14px] border border-hairline rounded-sm font-inter focus:border-ink-100 text-ink-50 outline-none',
+          )}
+          ref={ref}
+          {...registration}
+          {...props}
+        />
+      </FieldWrapper>
+    );
+  },
+);
+
+Input.displayName = 'Input';
+
+export { Input };
