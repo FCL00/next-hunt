da3d72f08db4890a0bc1468c8c87b6f8d35ec284
FCL00 <flagahit00@gmail.com>
2026-08-19 06:29:30 +0800

    feature: add new forms for sign-in and sign-up pages

---
+++ src/features/auth/components/signup-form.tsx
@@ -0,0 +1,87 @@
+'use client';
+import { paths } from '@/config/paths';
+import { Link } from '@/components/ui/link';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/forms';
+import { type SignUpInput, signUpInputSchema } from '@/validators/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { signUp } from '@/lib/auth-client';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+
+export default function SignUpForm() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpInput>({
+    resolver: zodResolver(signUpInputSchema),
+  });
+
+  const onSubmit = async (data: SignUpInput) => {
+    const { confirmPassword, ...credentials } = data;
+    await signUp.email(credentials, {
+      onSuccess: () => {
+        toast.success('Successfully created an account!');
+        router.push(paths.auth.signIn.getHref());
+      },
+      onError: (ctx) => {
+        toast.error(ctx.error.message);
+      },
+    });
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="sm:w-full md:max-w-md lg:max-w-lg">
+        <h1 className="max-w-sm">Get Started!</h1>
+        <p className="mb-8 text-[14px] text-ink-100">
+          Already have an Account?{' '}
+          <Link className="text-ink-50 border-b border-b-ink-50" href={paths.auth.signIn.getHref()}>
+            Login in here
+          </Link>
+        </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            type="name"
+            label="Name"
+            registration={register('name')}
+            error={errors.name}
+            placeholder="Enter your display name"
+          />
+          <Input type="email" label="Email" registration={register('email')} error={errors.email} placeholder="you@example.com" />
+          <Input
+            type="password"
+            label="Password"
+            registration={register('password')}
+            error={errors.password}
+            placeholder="••••••••"
+          />
+          <Input
+            type="password"
+            label="Confirm Password"
+            registration={register('confirmPassword')}
+            error={errors.confirmPassword}
+            placeholder="••••••••"
+          />
+          <Button type="submit" className="w-full text-[14.5px] p-[13px_16px]">
+            Create Account
+          </Button>
+          <div className="mt-7 text-[12px] text-ink-200 leading-[1.6] text-center">
+            By continuing you agree to Meridian's{' '}
+            <Link variant="muted" href={'/terms'}>
+              Terms
+            </Link>{' '}
+            and{' '}
+            <Link variant="muted" href="/privacy">
+              Privacy Policy
+            </Link>
+            .
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
