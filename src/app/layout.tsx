453862860fbf8caa01ce14a933575a5b70360881
FCL00 <flagahit00@gmail.com>
2026-08-18 16:30:03 +0800

    chore: remove old font and add new fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Mono, Fraunces, Inter } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const fontIbmMono = IBM_Plex_Mono({
+ weight: ['200', '300', '400', '500'],
+ variable: '--font-mono',
+});
+
+const fontFraunces = Fraunces({
+  variable: '--font-Fraunces',
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const fontInter = Inter({
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -21,7 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${fontIbmMono.variable} ${fontFraunces.variable} ${fontInter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
