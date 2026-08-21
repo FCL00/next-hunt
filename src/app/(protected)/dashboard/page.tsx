4770d15096adf762dcadb76446c92e33626361f4
FCL00 <flagahit00@gmail.com>
2026-08-21 17:35:51 +0800

    feature: replace sign out button implementation with SignOut component

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,25 +1,10 @@
-'use client';
-import { signOut } from '@/lib/auth-client';
-import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
-import { paths } from '@/config/paths';
+import { SignOutButton as SignOut } from '../_components/signout-button';
 
 export default function page() {
-  const router = useRouter();
-  const onSignOut = async () => {
-    await signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.replace(paths.auth.signIn.getHref());
-        },
-      },
-    });
-  };
-
   return (
     <div>
       Welcome to dashboard page
-      <Button onClick={onSignOut}>Sign Out</Button>
+      <SignOut />
     </div>
   );
 }
