556ef7bf55e48cd49d4d6819f05c3eb132e8ca0a
FCL00 <flagahit00@gmail.com>
2026-08-24 05:17:31 +0800

    feature: add ScrollArea component and integrate it into ApplicationColumn for better scrolling experience

--- src/features/applications/components/application-column.tsx
+++ src/features/applications/components/application-column.tsx
@@ -2,6 +2,7 @@ import { Badge } from '@/components/ui/badge';
 import { Card, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { type Application, ApplicationStage } from '@/validators/application';
 import { ApplicationDropdownMenu as Menu } from './application-dropdown-menu';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 type ApplicationColumnProps = {
   stage: ApplicationStage;
@@ -16,18 +17,20 @@ export function ApplicationColumn({ stage, applications }: ApplicationColumnProp
         <p className="font-mono text-xs text-dark-400">{stage}</p>
         <Badge className="bg-dark-800 rounded-full px-2">{stageApplication.length}</Badge>
       </div>
-      {stageApplication.map((application) => (
-        <Card className="relative mb-4" key={application.id}>
-          <CardHeader>
-            <CardTitle>{application.company}</CardTitle>
-            <CardDescription>{application.role}</CardDescription>
-            <span className="text-xs font-mono text-ink-muted">{application.salary}</span>
-            <div className="absolute top-1 right-2">
-              <Menu applicationId={application.id} currentStage={application.state} />
-            </div>
-          </CardHeader>
-        </Card>
-      ))}
+      <ScrollArea className='max-h-150'>
+        {stageApplication.map((application) => (
+          <Card className="relative mb-4" key={application.id}>
+            <CardHeader>
+              <CardTitle>{application.company}</CardTitle>
+              <CardDescription>{application.role}</CardDescription>
+              <span className="text-xs font-mono text-ink-muted">{application.salary}</span>
+              <div className="absolute top-1 right-2">
+                <Menu applicationId={application.id} currentStage={application.state} />
+              </div>
+            </CardHeader>
+          </Card>
+        ))}
+      </ScrollArea>
     </div>
   );
 }
